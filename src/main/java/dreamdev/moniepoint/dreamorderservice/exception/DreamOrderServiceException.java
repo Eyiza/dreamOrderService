775f3e12039781e1464cbaf0c51b5c9f85ceb3d8
Precious Michael <precious.michael2002@gmail.com>
2026-04-23 16:30:27 +0100

    project structure setup

---
+++ src/main/java/dreamdev/moniepoint/dreamorderservice/exception/DreamOrderServiceException.java
@@ -0,0 +1,7 @@
+package dreamdev.moniepoint.dreamorderservice.exception;
+
+public class DreamOrderServiceException extends RuntimeException {
+    public DreamOrderServiceException(String message) {
+        super(message);
+    }
+}
