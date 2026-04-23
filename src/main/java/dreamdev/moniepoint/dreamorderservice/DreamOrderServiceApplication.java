7260a96d32734785fddeba85c51215ffd78c1110
Precious Michael <precious.michael2002@gmail.com>
2026-04-23 16:14:32 +0100

    initialize project

---
+++ src/main/java/dreamdev/moniepoint/dreamorderservice/DreamOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package dreamdev.moniepoint.dreamorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dreamdev/moniepoint/dreamorderservice/DreamOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dreamdev.moniepoint.dreamorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
